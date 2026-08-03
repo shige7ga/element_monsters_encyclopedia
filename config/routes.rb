d9c16fdbb826aa47e101371e3cceb4bfe1d6f282
shige7ga <ameft_80@yahoo.co.jp>
2026-08-03 20:05:52 +0900

    認証後のマイページ遷移を実装

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get "mypage", to: "users#show", as: :mypage
+
   # Defines the root path route ("/")
   root "home#index"
 end
