b51dde6a6b6d65d8e2bc79067d2629db4c7f0471
shige7ga <ameft_80@yahoo.co.jp>
2026-08-05 22:59:44 +0900

    Fix RuboCop offenses

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,7 +1,7 @@
 class ElementsController < ApplicationController
   def index
     @elements = Element.order(:atomic_number)
-    @elements_by_position = @elements.where.not(atomic_number: 58..71).where.not(atomic_number: 90..103).index_by { |element| [element.period, element.group_number] }
+    @elements_by_position = @elements.where.not(atomic_number: 58..71).where.not(atomic_number: 90..103).index_by { |element| [ element.period, element.group_number ] }
     @lanthanides = @elements.where(atomic_number: 57..71)
     @actinides = @elements.where(atomic_number: 89..103)
   end
