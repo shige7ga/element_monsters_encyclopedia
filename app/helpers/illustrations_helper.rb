9120d496937f591296b629b9c06ede6d8a256722
shige7ga <ameft_80@yahoo.co.jp>
2026-08-19 23:05:26 +0900

    イラスト画像のOGPサイズを設定するcloudinaryURLを設定

--- app/helpers/illustrations_helper.rb
+++ app/helpers/illustrations_helper.rb
@@ -1,7 +1,13 @@
 module IllustrationsHelper
   def og_image_url(image)
     if image.blob.service_name == "cloudinary"
-      image.blob.url
+      cloudinary_url(
+        image.blob.key,
+        width: 1200,
+        height: 630,
+        crop: "pad",
+        background: "#020617"
+      )
     else
       polymorphic_url(image)
     end
